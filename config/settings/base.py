9b2586bf7cd19ecacc4bd48563867708593dd51a
Thitphavanh Phongsavath <thitphavanh.hery@gmail.com>
2025-12-15 16:47:02 +0700

    feat: allow `SESSION_COOKIE_SECURE` and `CSRF_COOKIE_SECURE` to be configured via environment variables.

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,6 +94,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
@@ -102,6 +103,7 @@ STATICFILES_DIRS = [
 
 # Media files settings
 MEDIA_URL = "/media/"
+
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
